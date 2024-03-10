b7224238f5a9b340ba48d2d68cbf80ad91c3b88a
rafiafa <161972960+rafiafa@users.noreply.github.com>
2024-03-10 18:41:19 -0500

    Create HomeMap.tsx

---
+++ components/homeComponents/HomeMap.tsx
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    <h2> UTA Datathon Location:</h2>
+    <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=13xEBo4ThnF3RQLGYzVbiYwh-kXLj3Jo&ehbc=2E312F&noprof=1" width="640" height="480"></iframe></p>
+</body>
+</html>
