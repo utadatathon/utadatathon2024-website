c6f4f92f69c1535cdfdb1760518c849a24a98e67
Pistonamey <ameyshinde11111@gmail.com>
2024-03-12 12:54:07 -0500

    version 1

--- components/homeComponents/HomeFooter.tsx
+++ components/homeComponents/HomeFooter.tsx
@@ -1,26 +1,28 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import { LinkedIn } from '@mui/icons-material';
 
 export default function HomeFooter() {
   return (
     <section className=" mt-16 px-6 py-8 md:text-base text-xs">
       <hr className="my-4 bg-complementary" />
       <div className="flex flex-col items-center justify-center gap-2 text-complementary text-center">
-        <div className="text-base md:text-lg">
-          {' '}
-          &copy; UTA Datathon 2024
-        </div>
-        <div className="text-[0.6rem] md:text-sm">
-          Designed by <p className="font-black inline">UTA Datathon Development Team | </p>
+      <div className="title-container"> {/* Flex container for titles */}
+      <h1 className="title text-primaryDark ">&copy;</h1>
+    <h1 className="title text-primaryDark h1-custom-font">UTA Datathon</h1>
+    <h1 className="title text-primaryDark">2024</h1>
+  </div>
+        <div className="text-[0.6rem] md:text-sm ">
+          Designed by <p className="font-black inline h1-custom-font">UTA Datathon Development Team | </p>
           {/* PLEASE DO NOT CHANGE <3 */}
-          HackPortal developed with &lt;3 by <p className="font-black inline">HackUTD</p> and{' '}
+          HackPortal developed with &lt;3 by <p className="font-black inline h1-custom-font">HackUTD</p> and{' '}
           <p className="font-black inline">ACM Development</p>
           {/* PLEASE DO NOT CHANGE <3 */}
         </div>
 
         
-        <div className="flex flex-row justify-center items-center space-x-6">
+        <div className="flex flex-row justify-center items-center space-x-6 h1-custom-font">
           <a
             href="https://catalog.uta.edu/academicregulations/dishonesty/"
             target="_blank"
@@ -78,6 +80,9 @@ export default function HomeFooter() {
           </a>
           <a href="" rel="noopener noreferrer" target="_blank">
             <FacebookIcon className="footerIcon" />
+          </a>
+          <a href="" rel="noopener noreferrer" target="_blank">
+            <LinkedIn className="footerIcon" />
           </a>
         </div>
       </div>
--- components/homeComponents/HomeHero.tsx
+++ components/homeComponents/HomeHero.tsx
@@ -1,21 +1,35 @@
 import { useRouter } from 'next/router';
 import { buttonDatas } from '../../lib/data';
+import Image from 'next/image'
+import { useState, useEffect } from 'react';
 
 export default function HomeHero() {
   const router = useRouter();
+  const [imageLoaded, setImageLoaded] = useState(false);
+ 
+
+  
 
   return (
-    <section className="min-h-screen p-4 bg-contain bg-customBackground">
+    <section className="min-h-screen p-4 bg-contain bg-black">
       <div
-        style={{ minHeight: 480 }}
-        className="max-w-4xl mx-auto flex flex-col justify-center items-center"
-      >
-        <h1 className="text-center md:text-8xl text-6xl font-bold text-primaryDark">UTA Datathon 2024</h1>{' '}
+        style={{ maxHeight: 700 }}
+        className="max-w-4xl mx-auto flex flex-wrap justify-center items-center"      >
+<div className="video-container relative w-full flex justify-center items-center" >          
+  <video width="920" height="340" autoPlay>
+    <source src="/assets/my_first_vr_video.mp4" type="video/mp4" />
+  </video>
+</div>
+
+
+
+        {/* <h1 className="text-center md:text-8xl text-6xl font-bold text-primaryDark h1-custom-font" >UTA Datathon</h1> */}
+        {/* <h1 className="text-center md:text-8xl text-6xl font-bold text-primaryDark" >2024</h1>{' '} */}
         {/* !change */}
-        <p className="text-center my-4 font-semibold md:text-xl text-md text-primaryDark opacity-80">
-          {' '}
-          {/* !change */}Powered by UTA Libraries
-        </p>
+        {/* <p className="text-center my-4 font-semibold md:text-xl text-md text-primaryDark opacity-80 h1-custom-font"> */}
+          {/* {' '} */}
+          {/* !changePowered by UTA Libraries */}
+        {/* </p> */}
       </div>
       {/* TODO: Programmatically show these based on configured times/organizer preference */}
 
@@ -24,7 +38,7 @@ export default function HomeHero() {
           <button
             key={button.text}
             onClick={() => router.push(button.path)}
-            className="max-w-[14rem] w-[14rem] md:max-w-full bg-white py-4 rounded-xl h-10 flex items-center justify-center font-semibold text-xl text-primaryDark border-2 border-gray-300"
+            className="max-w-[14rem] w-[14rem] md:max-w-full bg-black py-4 rounded-xl h-10 flex items-center justify-center font-semibold h1-custom-font text-xl text-primaryDark border-2 border-gray-300"
           >
             {button.text}
           </button>
