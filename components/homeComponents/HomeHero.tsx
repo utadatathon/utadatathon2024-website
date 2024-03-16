6a9a8bf5349dd6e0b30e32d007ce6bb876e5f4c9
Pistonamey <ameyshinde11111@gmail.com>
2024-03-16 11:21:55 -0500

    changed animation to typo

--- components/homeComponents/HomeHero.tsx
+++ components/homeComponents/HomeHero.tsx
@@ -1,54 +1,84 @@
 import { useRouter } from 'next/router';
-import { buttonDatas } from '../../lib/data';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import { buttonDatas } from '../../lib/data';
 
 export default function HomeHero() {
   const router = useRouter();
   const [showVideo, setShowVideo] = useState(false);
   const [showText, setShowText] = useState(false);
 
-  // Function to call to show the text
   const handleUserInteraction = () => {
-    setShowVideo(false); // Ensure video is not displayed
-    setShowText(true); // Trigger text display
+    setShowVideo(false);
+    setShowText(true);
   };
 
   useEffect(() => {
-    // Add event listeners when component mounts
     window.addEventListener('click', handleUserInteraction);
     window.addEventListener('scroll', handleUserInteraction, { passive: true });
 
-    // Cleanup event listeners when component unmounts
     return () => {
       window.removeEventListener('click', handleUserInteraction);
       window.removeEventListener('scroll', handleUserInteraction);
     };
   }, []);
 
+  const calculateTimeLeft = () => {
+    const difference = +new Date('04/10/2024') - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
   return (
     <section className="min-h-screen p-4 bg-contain bg-black">
-    <div
-      style={{ maxHeight: 920 }}
-      className="max-w-4xl mx-auto flex flex-wrap justify-center items-center"
-    >
-      <div className="relative w-full flex justify-center items-center">
-        <Image
-          src="/assets/my_first_vr_photo.png"
-          alt="VR Image"
-          width={920}
-          height={510}
-          onLoadingComplete={() => setShowText(false)}
-        />
-        {showText && (
-          <>
-            <p className="typewriter">&nbsp;&nbsp;UTA DATATHON</p>
-            <p className="typewriter2">&nbsp;&nbsp;&nbsp;2024</p>
-          </>
-        )}
+      <div style={{ maxHeight: 920 }} className="max-w-4xl mx-auto flex flex-wrap justify-center items-center">
+        <div className="relative w-full flex justify-center items-center">
+          <Image
+            src="/assets/my_first_vr_photo.png"
+            alt="VR Image"
+            width={1240}
+            height={830}
+            onLoadingComplete={() => setShowText(false)}
+          />
+          {showText && (
+            <>
+              <p className="typewriter">&nbsp;&nbsp;UTA DATATHON</p>
+              <p className="typewriter2">&nbsp;&nbsp;&nbsp;2024</p>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-      <div className="flex flex-col items-center md:flex-row md:justify-around px-44 md:space-y-0 space-y-3">
+      {/* Countdown Timer */}
+      <div className="countdown-timer text-primaryDark" style={{ textAlign: 'center', margin: '40px 0' }}>
+  <h2 className="h1-custom-font">Countdown to the Datathon</h2>
+  <div className="time-left text-primaryDark " style={{ display: 'flex', justifyContent: 'center', gap: '15px', marginTop:'20px', flexWrap: 'wrap' }}>
+    {Object.keys(timeLeft).map((interval) => (
+      <div key={interval} className="countdown-box">
+        <span className="countdown-number">{timeLeft[interval]}</span>
+        <p className="countdown-label h1-custom-font">{interval.toUpperCase()}</p>
+      </div>
+    ))}
+  </div>
+</div>
+      <div className="flex flex-col items-center md:flex-row md:justify-around px-44 md:space-y-5 space-y-3">
         {buttonDatas.map((button) => (
           <button
             key={button.text}
