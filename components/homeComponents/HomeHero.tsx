6f4f498cd074fa2574c62b81145c7bb5331ec829
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-18 20:22:03 -0500

    update links in main

--- components/homeComponents/HomeHero.tsx
+++ components/homeComponents/HomeHero.tsx
@@ -89,21 +89,21 @@ export default function HomeHero() {
           <div className="relative flex flex-col items-center md:flex-row md:justify-around px-44 md:space-y-0 space-y-3 z-9 w-full">
             {/* Button 1 */}
             <button
-              onClick={() => router.push('/apply')}
+              onClick={() => router.push('/auth')}
               className="max-w-[14rem] w-[14rem] md:max-w-full backdrop-blur-sm bg-customBackground/30 py-4 rounded-xl h-10 flex items-center justify-center font-semibold text-xl text-primaryDark border-2 border-gray-300 custom-font"
             >
               Apply
             </button>
             {/* Button 2 */}
             <button
-              onClick={() => router.push('/discord')}
+              onClick={() => {window.open('https://discord.gg/Ypfa6zXCJ9', '_blank');}}
               className="max-w-[14rem] w-[14rem] md:max-w-full backdrop-blur-sm bg-customBackground/30 py-4 rounded-xl h-10 flex items-center justify-center font-semibold text-xl text-primaryDark border-2 border-gray-300 custom-font"
             >
               Discord
             </button>
             {/* Button 3 */}
             <button
-              onClick={() => router.push('/devpost')}
+              onClick={() => {window.open('https://uta-datathon-2024.devpost.com/', '_blank');}}
               className="max-w-[14rem] w-[14rem] md:max-w-full backdrop-blur-sm bg-customBackground/30 py-4 rounded-xl h-10 flex items-center justify-center font-semibold text-xl text-primaryDark border-2 border-gray-300 custom-font"
             >
               Dev Post
