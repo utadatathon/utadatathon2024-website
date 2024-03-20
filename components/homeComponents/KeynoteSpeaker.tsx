6686650ecdbba9fe26a675a58338994661459e8f
Atiqur Rahman <atiqur.main@gmail.com>
2024-03-20 02:33:10 -0500

    update speakers and judges section

--- components/homeComponents/KeynoteSpeaker.tsx
+++ components/homeComponents/KeynoteSpeaker.tsx
@@ -25,7 +25,7 @@ export default function KeynoteSpeaker(props) {
   }, []);
 
   return (
-    <div className="group flex flex-col items-center w-72 h-72 sm:h-80 rounded-xl shadow-xl relative transition duration-500 ease-in-out overflow-hidden bg-secondary text-white">
+    <div className="group flex flex-col items-center w-72 h-72 sm:h-80 rounded-xl shadow-xl relative transition duration-500 ease-in-out overflow-hidden bg-indigo-300 text-white">
       <div className="rounded-t-sm">
         {props.imageLink !== undefined && imageLink !== undefined && (
           <Image
@@ -38,13 +38,13 @@ export default function KeynoteSpeaker(props) {
           />
         )}
       </div>
-      <div className="flex-col items-center justify-center w-full h-72 sm:h-80 absolute">
+      <div className="flex-col items-center justify-center w-full h-72 sm:h-80 absolute translate-y-56 sm:translate-y-64 group-hover:translate-y-0 transition duration-500 ease-in-out overflow-hidden">
         <div className="rounded-b-sm bg-gradient-to-br from-indigo-900 via-indigo-800 to-violet-800 p-2 font-semibold">
-          <h1 className="text-lg font-extrabold p-2 text-center text-complementary bg-gradient-to-br from-fuchsia-300 to-purple-200 bg-clip-text text-transparent">{props.name}</h1>
-          <h1 className="text-base mt-2 p-2">{props.subtitle}</h1>
+          <h1 className="text-lg font-extrabold p-2 text-white">{props.name}</h1>
+          <h1 className="text-sm p-2 text-complementary/60 font-medium text-justify">{props.subtitle}</h1>
           {/* show description on hover by sliding up */}
-          <div className="sm:h-72 h-80 text-justify font-normal overflow-y-scroll p-2">
-            <p className="text-[13px] pt-2 pb-24 sm:pb-10">{props.description}</p>
+          <div className="opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out sm:h-72 h-80 font-normal overflow-y-scroll p-2">
+            <p className="text-lg font-extrabold pt-2 pb-24 sm:pb-10 text-complementary bg-gradient-to-br from-fuchsia-300 to-purple-200 bg-clip-text text-transparent">{props.description}</p>
           </div>
         </div>
       </div>
