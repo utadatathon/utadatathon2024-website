3f8b0b01bfcc44b57bbceda2728823b4176765b8
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-01 14:05:34 -0500

    fix spacing for sponsors and map buttons

--- components/homeComponents/HomeSponsors.tsx
+++ components/homeComponents/HomeSponsors.tsx
@@ -28,7 +28,7 @@ export default function HomeSponsors(props: { sponsorCard: Sponsor[] }) {
           
           {/* Sponsor Card */}
           <section className="flex flex-wrap justify-center p-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 md:gap-y-4 lg:gap-y-4 gap-y-0">
               {sponsor.map(({ link, reference }, idx) => (
                 <SponsorCard key={idx} link={link} reference={reference} />
               ))}
